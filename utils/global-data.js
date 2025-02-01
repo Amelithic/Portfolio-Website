4edf010c9fe13296f5960bd9da5d567889b650f4
Amelithic <61317512+Amelithic@users.noreply.github.com>
2025-02-01 20:40:12 +0000

    Minor changes to global var values and comments

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,15 +1,17 @@
 export const getGlobalData = () => {
+  // SET GLOBAL VARS HERE
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : 'Amelithic.com';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'Portfolio website based on Next.js';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.';
 
   return {
+    //RETURN ALL GLOBAL VARS
     name,
     blogTitle,
     footerText,
