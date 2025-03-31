acc3b5b9c5a11d8d6a40dea618afa39e78747533
Amelithic <amelie.mccarthy058@gmail.com>
2025-03-31 22:46:39 +0100

    index.js portfolio link changes

--- pages/index.js
+++ pages/index.js
@@ -152,6 +152,7 @@ const Index = ({ posts, globalData, initialCareerIndex, initialCareerTitle, init
         <section className={styles.indexItems}>
           <p>section 2: skills</p>
           <p>section 4: blog</p>
+          <Link href='../public/PDFs/Technical Portfolio Brochure.pdf' title='Technical Portfolio Brochure'>Link to my tech-related portfolio</Link>
         </section>
 
         <section className={styles.indexItems}>
